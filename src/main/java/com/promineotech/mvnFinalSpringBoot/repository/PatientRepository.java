8a234759fbe04fdaa66e2a827797bf31ba980b3a
Cesar Pacheco <cpacheco@stchome.com>
2020-10-01 14:42:57 -0700

    first commit

--- src/main/java/com/promineotech/mvnFinalSpringBoot/repository/PatientRepository.java
+++ src/main/java/com/promineotech/mvnFinalSpringBoot/repository/PatientRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
+
 public interface PatientRepository extends CrudRepository<Patient, Long> {
 	List<Patient> findAll();
 
